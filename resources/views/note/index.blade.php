326d3c1b90675d0a2c796a5ec501ecf34ac3179b
jericho.yu.llanes <jericho.yu.llanes@gmail.com>
2024-12-08 02:19:02 +0800

    Render Notes and Include CSS and JS Files

--- resources/views/note/index.blade.php
+++ resources/views/note/index.blade.php
@@ -1,5 +1,26 @@
 <x-layout>
     <!-- Breathing in, I calm body and mind. Breathing out, I smile. - Thich Nhat Hanh -->
+    <div class="note-container">
 
-    <h1>Index</h1>
+        {{-- Two ways to have custome links for a tags' href attribute --}}
+        {{-- <a href="/note/create" class="new-note-button"> --}}
+        {{-- or like this below:
+    --}}<a href="{{ route('note.create') }}" class="new-note-btn">
+            New Note
+        </a>
+        <div class="notes">
+            @foreach ($notes as $note)
+                <div class="note">
+                    <div class="note-body">
+                        {{ Str::words($note->note, 30) }}
+                    </div>
+                    <div class="note-buttons">
+                        <a href="{{ route('note.show', $note) }}" class="note-show-button">View</a>
+                        <a href="{{ route('note.edit', $note) }}" class="note-edit-button">Edit</a>
+                        <button class="note-delete-button">Delete</button>
+                    </div>
+                </div>
+            @endforeach
+        </div>
+    </div>
 </x-layout>

9c761e18056a274f90a8026d9c1dbb8c213b4173
jericho.yu.llanes <jericho.yu.llanes@gmail.com>
2024-12-08 03:14:50 +0800

    Defined Views

--- resources/views/note/show.blade.php
+++ resources/views/note/show.blade.php
@@ -1,5 +1,18 @@
 <x-layout>
     <!-- Because you are alive, everything is possible. - Thich Nhat Hanh -->
 
-    <h1>Show</h1>
+    <div class="note-container single-note">
+        <div class="note-header">
+            <h1>Note: {{ $note->created_at }}</h1>
+            <div class="note-buttons">
+                <a href="{{ route('note.edit', $note) }}" class="note-edit-button">Edit</a>
+                <button class="note-delete-button">Delete</button>
+            </div>
+        </div>
+        <div class="note">
+            <div class="note-body">
+                {{ $note->note }}
+            </div>
+        </div>
+    </div>
 </x-layout>

85a5449cfcd1951e0403dbbc9c3327271b05a214
jericho.yu.llanes <jericho.yu.llanes@gmail.com>
2024-12-08 08:18:15 +0800

    Added Custom Forbidden View Page

---
+++ resources/views/errors/403.blade.php
@@ -0,0 +1,7 @@
+<x-app-layout>
+    <div class="container mx-auto text-center py-12">
+        <h1 class="red">Forbidden Page</h1>
+        <p>You do not have permission to access this page.</p>
+        <p>This content is restricted to authorized users only.</p>
+    </div>
+</x-app-layout>
